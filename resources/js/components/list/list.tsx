54417fc065bb1b81ed03bce5a5dc73e77fee0911
gergo_f <tamas.gergely86@gmail.com>
2025-11-27 10:38:42 +0100

    Fix: Remove list margin

--- resources/js/components/list/list.tsx
+++ resources/js/components/list/list.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 
 export default function List({ className, children }: { className?: string, children: ReactNode }) {
     return (
-        <div className={cn('mt-5 text-sm bg-list p-2 mx-2 rounded-lg md:px-5 lg:mx-0', className)}>
+        <div className={cn('mt-5 text-sm bg-list p-2 rounded-lg md:px-5 lg:mx-0', className)}>
             {children}
         </div>
     );
